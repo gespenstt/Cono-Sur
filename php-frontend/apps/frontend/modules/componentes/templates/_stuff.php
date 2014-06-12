d76e8859984592d8a41e7360f8bea068c2a921f5
invabot <invasor@gmail.com>
2014-06-12 19:44:39 -0400

    mejoras modal
    agregue banderitas

--- php-frontend/apps/frontend/modules/componentes/templates/_stuff.php
+++ php-frontend/apps/frontend/modules/componentes/templates/_stuff.php
@@ -5,22 +5,19 @@
     <div class="modal-content">
       <div class="modal-header">
         <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
-        <h4 class="modal-title" id="myModalLabel">Welcome to Cono Sur´s Blogger Competition!
- 
-Although your country is not participating this year, we invite you to check out the site and stay tuned for heaps of savory recipes to pair with your favorite Cono Sur wines, which we will publish 18 August, 2014. 
- 
-Cheers!</h4>
+        <h4 class="modal-title" id="myModalLabel">Welcome to Cono Sur´s Blogger Competition!</h4>
       </div>
+
       <div class="modal-body">
 
         <p>Although your country is not participating this year, we invite you to check out the site and stay tuned for heaps of savory recipes to pair with your favorite Cono Sur wines, which we will publish 18 August, 2014.</p> 
 
-        <p></p>
+        <img src="<?=public_path("img/country-modal.png");?>" alt="">
  
         <p>Cheers!</p>
-
           
           <input type="hidden" id="defaultLang" value="<?=url_for("home/lang/?set=en");?>" />
+
       </div>
     </div>
   </div>
