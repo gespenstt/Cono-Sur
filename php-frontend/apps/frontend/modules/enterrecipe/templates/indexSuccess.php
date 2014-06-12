b181cfd78c53b25c48a4c43db1b6ab6754d73b92
Jaime Vasquez <rodrigoxv@gmail.com>
2014-06-12 19:18:48 -0400

    Update

--- php-frontend/apps/frontend/modules/enterrecipe/templates/indexSuccess.php
+++ php-frontend/apps/frontend/modules/enterrecipe/templates/indexSuccess.php
@@ -7,13 +7,10 @@
 						<div class="col-md-12 ">
 
 
-							<h1>ENTER YOUR RECIPE</h1>
+							<h1><?=$array_diccionario["panel_a"]["titulo"];?></h1>
 
 							<p class="text-14-uppercase ">
-								ARE YOU A FOOD AND WINE AND/OR LIFESTYLE BLOGGER FROM ENGLAND?<br />
-								IF YES, CREATE A RECIPE UTILIZING XXXXX AS THE MAIN INGREDIENT THAT PAIRS PERFECTLY WITH OUR<br />
-								CONO SUR PINOT NOIR AND YOU COULD WIN A TRIP FOR TWO TO CHILE!<br />
-								ALL ENTRIES MUST BE RECEIVED BY 11 AUGUST, 2014.
+								<?=$array_diccionario["panel_a"]["texto"];?>
 							</p>
 
 						</div>
@@ -34,49 +31,49 @@
                                                     <form action="<?=url_for("enterrecipe/guardar");?>" method="post" class="form-horizontal enter-recipe-form" role="form" onsubmit="return validarRecipe();">
 
 							 	<div class="form-group">
-							    	<label for="inputEmail3" class="col-sm-2 control-label">Recipe Name</label>
+							    	<label for="inputEmail3" class="col-sm-2 control-label"><?=$array_diccionario["formulario"]["recipe_name"];?></label>
 									<div class="col-sm-10">
 										<input type="text" class="form-control" data-msg="Debe ingresar Recipe name" name="nombre_receta" id="nombre_receta">
 									</div>
 								</div>
 
 								<div class="form-group">
-							    	<label for="inputEmail3" class="col-sm-2 control-label">Ingredients</label>
+							    	<label for="inputEmail3" class="col-sm-2 control-label"><?=$array_diccionario["formulario"]["ingredients"];?></label>
 									<div class="col-sm-10">
 										<input type="text" class="form-control" data-msg="Debe ingresar Ingredients" name="ingredientes" id="ingredientes" placeholder="EXAMPLE: Ingredient 1, Ingredient 2">
 									</div>
 								</div>
 
 								<div class="form-group">
-							    	<label for="inputEmail3" class="col-sm-2 control-label">Cooking Instructions</label>
+							    	<label for="inputEmail3" class="col-sm-2 control-label"><?=$array_diccionario["formulario"]["cooking_instructions"];?></label>
 									<div class="col-sm-10">
 										<textarea class="form-control" rows="3" data-msg="Debe ingresar Cooking instructions" name="intrucciones" id="intrucciones"></textarea>
 									</div>
 								</div>
 
 								<div class="form-group">
-							    	<label for="inputEmail3" class="col-sm-2 control-label">Wine Used (specify range & variety)</label>
+							    	<label for="inputEmail3" class="col-sm-2 control-label"><?=$array_diccionario["formulario"]["wine_used"];?>Wine Used (specify range & variety)</label>
 									<div class="col-sm-10">
 										<input type="text" class="form-control" data-msg="Debe ingresar wine used" id="vino_usado" name="vino_usado" placeholder="EXAMPLE: Cono Sur, Ocio, Pinot Noir">
 									</div>
 								</div>
 
 								<div class="form-group">
-							    	<label for="inputEmail3" class="col-sm-2 control-label">Your Name</label>
+							    	<label for="inputEmail3" class="col-sm-2 control-label"><?=$array_diccionario["formulario"]["your_name"];?>Your Name</label>
 									<div class="col-sm-10">
                                                                             <input type="text" class="form-control" data-msg="Debe ingresar your name" name="nombre" id="nombre">
 									</div>
 								</div>
 
 								<div class="form-group">
-							    	<label for="inputEmail3" class="col-sm-2 control-label">Link to your blog</label>
+							    	<label for="inputEmail3" class="col-sm-2 control-label"><?=$array_diccionario["formulario"]["link_to_your_blog"];?>Link to your blog</label>
 									<div class="col-sm-10">
 										<input type="text" class="form-control" data-msg="Debe ingresar link to your blog" id="link_blog" name="link_blog">
 									</div>
 								</div>
 
 								<div class="form-group">
-							    	<label for="inputEmail3" class="col-sm-2 control-label">Your E-mail</label>
+							    	<label for="inputEmail3" class="col-sm-2 control-label"><?=$array_diccionario["formulario"]["your_email"];?>Your E-mail</label>
 									<div class="col-sm-10">
 										<input type="email" class="form-control" data-msg="Debe ingresar un email valido" id="email" name="email">
 									</div>
@@ -87,14 +84,14 @@
 							    	<div class="checkbox">
 										<label>
 											<input type="checkbox" name="acepta_pais" data-msg="Debe aceptar el tos pais" id="acepta_pais">
-									    I hereby verify that I am of legal drinking age in my country of residence
+									    <?=$array_diccionario["formulario"]["tos1"];?>
 									  </label>
 									</div>
 
 									<div class="checkbox">
 										<label>
 									    	<input type="checkbox" name="acepta_tos" data-msg="Debe aceptar el tos 2 " id="acepta_tos">
-									    I hereby verify that I have read and fully understand the <a href="" class="link-georgia">Blogger Competition ́s Rules and Regulations.</a>
+									    <?=$array_diccionario["formulario"]["tos2"];?> <a href="" class="link-georgia"><?=$array_diccionario["formulario"]["link_tos2"];?></a>
 										</label>
 									</div>
 
