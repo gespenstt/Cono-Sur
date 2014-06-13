42f2f78e3772a77dcfd17ae05b78ee604d7c246c
Jaime Vasquez <rodrigoxv@gmail.com>
2014-06-12 23:40:16 -0400

    Update

--- php-frontend/web/js/base.js
+++ php-frontend/web/js/base.js
@@ -10,9 +10,9 @@ $(document).ready(function(){
     //DET LANG
     if(!det_lang){
         $('#modalLang').modal(); 
-        $('#modalLang').on('hide.bs.modal', function () {
+        /*$('#modalLang').on('hide.bs.modal', function () {
             location.href=$("#defaultLang").val();
-        })
+        })*/
     }
     
     if(!$("#foto").length > 0){
