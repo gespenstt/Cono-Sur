faa98e4bda8c6777b93e964f9c7bd354baf683f4
invabot <invasor@gmail.com>
2014-06-12 21:31:22 -0400

    validaciones

--- php-frontend/apps/frontend/modules/enterrecipe/templates/indexSuccess.php
+++ php-frontend/apps/frontend/modules/enterrecipe/templates/indexSuccess.php
@@ -35,73 +35,73 @@
 							 	<div class="form-group">
 							    	<label for="inputEmail3" class="col-sm-2 control-label"><?=$array_diccionario["formulario"]["recipe_name"];?></label>
 									<div class="col-sm-10">
-										<input type="text" class="form-control" data-msg="Debe ingresar Recipe name" name="nombre_receta" id="nombre_receta">
+										<input type="text" class="form-control" data-msg="Please complete." name="nombre_receta" id="nombre_receta">
 									</div>
 								</div>
 
 								<div class="form-group">
 							    	<label for="inputEmail3" class="col-sm-2 control-label"><?=$array_diccionario["formulario"]["ingredients"];?></label>
 									<div class="col-sm-10">
-										<input type="text" class="form-control" data-msg="Debe ingresar Ingredients" name="ingredientes" id="ingredientes" placeholder="EXAMPLE: Ingredient 1, Ingredient 2">
+										<input type="text" class="form-control" data-msg="Please list ingredients." name="ingredientes" id="ingredientes" placeholder="EXAMPLE: Ingredient 1, Ingredient 2">
 									</div>
 								</div>
 
 								<div class="form-group">
 							    	<label for="inputEmail3" class="col-sm-2 control-label"><?=$array_diccionario["formulario"]["cooking_instructions"];?></label>
 									<div class="col-sm-10">
-										<textarea class="form-control" rows="3" data-msg="Debe ingresar Cooking instructions" name="intrucciones" id="intrucciones"></textarea>
+										<textarea class="form-control" rows="3" data-msg="Please list cooking instructions." name="intrucciones" id="intrucciones"></textarea>
 									</div>
 								</div>
 
 								<div class="form-group">
 							    	<label for="inputEmail3" class="col-sm-2 control-label"><?=$array_diccionario["formulario"]["wine_used"];?></label>
 									<div class="col-sm-10">
-										<input type="text" class="form-control" data-msg="Debe ingresar wine used" id="vino_usado" name="vino_usado" placeholder="EXAMPLE: Cono Sur, Ocio, Pinot Noir">
+										<input type="text" class="form-control" data-msg="Must name range and variety." id="vino_usado" name="vino_usado" placeholder="EXAMPLE: Cono Sur, Ocio, Pinot Noir">
 									</div>
 								</div>
 
 								<div class="form-group">
 							    	<label for="inputEmail3" class="col-sm-2 control-label">FOTO</label>
 									<div class="col-sm-10">
-                                                                            <canvas id="previewcanvas"></canvas>
-                                                                            <input type="file" name="foto" id="foto" onchange="return ShowImagePreview( this.files );" />
+                                        <canvas id="previewcanvas"></canvas>
+                                        <input type="file" name="foto" data-msg="Your photo does not meet the dimension requirements. Please try again with a photo of 400 x 400 pixels." id="foto" onchange="return ShowImagePreview( this.files );" />
 									</div>
 								</div>
 
 								<div class="form-group">
 							    	<label for="inputEmail3" class="col-sm-2 control-label"><?=$array_diccionario["formulario"]["your_name"];?></label>
 									<div class="col-sm-10">
-                                                                            <input type="text" class="form-control" data-msg="Debe ingresar your name" name="nombre" id="nombre">
+                                        <input type="text" class="form-control" data-msg="Please list complete name." name="nombre" id="nombre">
 									</div>
 								</div>
 
 								<div class="form-group">
 							    	<label for="inputEmail3" class="col-sm-2 control-label"><?=$array_diccionario["formulario"]["link_to_your_blog"];?></label>
 									<div class="col-sm-10">
-										<input type="text" class="form-control" data-msg="Debe ingresar link to your blog" id="link_blog" name="link_blog">
+										<input type="text" class="form-control" data-msg="Please include link." id="link_blog" name="link_blog">
 									</div>
 								</div>
 
 								<div class="form-group has-error">
 							    	<label for="inputEmail3" class="col-sm-2 control-label"><?=$array_diccionario["formulario"]["your_email"];?></label>
 									<div class="col-sm-10">
-										<input type="email" class="form-control" data-msg="Debe ingresar un email valido" id="email" name="email">
+										<input type="email" class="form-control" data-msg="Please follow proper format: xxxx@xxx.xxx" id="email" name="email">
 										<p class="text-danger">Validacion</p>
-									</div>
+									</div> 
 								</div>
 
 								<div class="form-group">
 
 							    	<div class="checkbox">
 										<label>
-											<input type="checkbox" name="acepta_pais" data-msg="Debe aceptar el tos pais" id="acepta_pais">
+											<input type="checkbox" name="acepta_pais" data-msg="Please verify age." id="acepta_pais">
 									    <?=$array_diccionario["formulario"]["tos1"];?>
 									  </label>
 									</div>
 
 									<div class="checkbox">
 										<label>
-									    	<input type="checkbox" name="acepta_tos" data-msg="Debe aceptar el tos 2 " id="acepta_tos">
+									    	<input type="checkbox" name="acepta_tos" data-msg="You must read and fully understand the rules and regulations." id="acepta_tos">
 									    <?=$array_diccionario["formulario"]["tos2"];?> <a href="" class="link-georgia"><?=$array_diccionario["formulario"]["link_tos2"];?></a>
 										</label>
 									</div>
