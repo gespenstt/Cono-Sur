be04df3baf795cd7748a2f62a7715b1f4c974315
invabot <invasor@gmail.com>
2014-06-12 20:43:51 -0400

    input parche

--- php-frontend/apps/frontend/modules/enterrecipe/templates/indexSuccess.php
+++ php-frontend/apps/frontend/modules/enterrecipe/templates/indexSuccess.php
@@ -109,7 +109,7 @@
 					
 							  	<div class="form-group">
 								    <div class="col-sm-offset-2 col-sm-10">
-								    	<button type="submit" >SUBMIT</button>
+								    	<button type="submit" class="btn btn-submit pull-right">SUBMIT</button>
 								    </div>
 							  	</div>
 
