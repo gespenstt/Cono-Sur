c73f6b020eeab6983b63686792e737d1a808bb75
invabot <invasor@gmail.com>
2014-06-12 20:40:37 -0400

    vuelta atras personalizacion input

--- php-frontend/apps/frontend/modules/enterrecipe/templates/indexSuccess.php
+++ php-frontend/apps/frontend/modules/enterrecipe/templates/indexSuccess.php
@@ -64,7 +64,7 @@
 							    	<label for="inputEmail3" class="col-sm-2 control-label">FOTO</label>
 									<div class="col-sm-10">
                                                                             <canvas id="previewcanvas"></canvas>
-                                                                            <input type="file" class="btn btn-submit" name="foto" id="foto" onchange="return ShowImagePreview( this.files );" />
+                                                                            <input type="file" name="foto" id="foto" onchange="return ShowImagePreview( this.files );" />
 									</div>
 								</div>
 
