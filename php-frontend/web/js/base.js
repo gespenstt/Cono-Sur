7e001c82d8c5d9353dddb461529688747c057f7a
Jaime Vasquez <rodrigoxv@gmail.com>
2014-06-12 23:39:10 -0400

    Update

--- php-frontend/web/js/base.js
+++ php-frontend/web/js/base.js
@@ -8,7 +8,7 @@ $(document).ready(function(){
     var msgError = "";
     
     //DET LANG
-    if(det_lang){
+    if(!det_lang){
         $('#modalLang').modal(); 
         $('#modalLang').on('hide.bs.modal', function () {
             location.href=$("#defaultLang").val();
