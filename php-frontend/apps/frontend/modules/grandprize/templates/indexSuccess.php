cae850c63c737d4de3f17b3d39dd5d805499be58
invabot <invasor@gmail.com>
2014-06-16 21:40:45 -0400

    altura eliminacionde padding

--- php-frontend/apps/frontend/modules/grandprize/templates/indexSuccess.php
+++ php-frontend/apps/frontend/modules/grandprize/templates/indexSuccess.php
@@ -5,9 +5,9 @@
 
 					<div class="row show-grid row-no-margin">
 
-						<div class="col-md-5 text-center">
+						<div class="col-md-5 text-center no-padding">
 
-							<div class="dark-grey grand-prize no-padding">
+							<div class="dark-grey grand-prize">
 
 								<h1><?=$array_diccionario["panel_a"]["titulo"];?></h1>
 
