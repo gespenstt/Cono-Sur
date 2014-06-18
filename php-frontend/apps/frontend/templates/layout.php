d89159bb266870c52f68877b15bfb0c21e322495
invabot <invasor@gmail.com>
2014-06-18 11:07:03 -0400

    cambio meta viewport

--- php-frontend/apps/frontend/templates/layout.php
+++ php-frontend/apps/frontend/templates/layout.php
@@ -4,8 +4,9 @@
             <title>Cono Sur</title>
             <meta charset="UTF-8">
             <meta name=description content="">
-            <meta name=viewport content="width=device-width, initial-scale=1">
-            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+            <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+
             <!-- Bootstrap CSS -->
             <link href="<?=public_path("css/bootstrap.css");?>" rel="stylesheet" media="screen">
             <link href="<?=public_path("css/main.css");?>" rel="stylesheet" media="screen">
