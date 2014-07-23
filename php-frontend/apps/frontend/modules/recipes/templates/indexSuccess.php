1e96c65c31b201dd6794c1dafad2b895b5887e43
invabot <invabot>
2014-07-22 22:13:33 -0400

    nuevo link ver receta

--- php-frontend/apps/frontend/modules/recipes/templates/indexSuccess.php
+++ php-frontend/apps/frontend/modules/recipes/templates/indexSuccess.php
@@ -17,21 +17,18 @@
 
 						<div class="col-md-6">
 
-							<a href="recipe_detail.html">
-
-								<ul class="recipe-item green">
-									<li class="recipe-img" style="background-image: url(<?=public_path("img/recipe01.jpg");?>)"></li>
-									<li class="recipe-name txt-center">
-										<h1>Paula Troncoso</h1> 
-										<p>CORDERO CON FRITAS EN SALSA FRAMBUESA</p>
-									</li>
-									<li class="recipe-wine txt-center light">
-										<span class="icon-glass"></span>
-										<h2>Cono Sur riestling</h2>
-									</li>
-								</ul>
-
-							</a>	
+							<ul class="recipe-item green">
+								<li class="recipe-img" style="background-image: url(<?=public_path("img/recipe01.jpg");?>)"></li>
+								<li class="recipe-name txt-center">
+									<h1>Paula Troncoso</h1> 
+									<p>CORDERO CON FRITAS EN SALSA FRAMBUESA</p>
+									<a href=""><h1>View Recipe</h1></a> 
+								</li>
+								<li class="recipe-wine txt-center light">
+									<span class="icon-glass"></span>
+									<h2>Cono Sur riestling</h2>
+								</li>
+							</ul>
 							
 						</div>
 
@@ -116,7 +113,7 @@
 									<h1>Paula Troncoso</h1> 
 									<p>CORDERO CON FRITAS EN SALSA FRAMBUESA</p>
 								</li>
-								<li class="recipe-wine txt-center green-light">
+								<li class="recipe-wine txt-center light">
 									<span class="icon-glass"></span>
 									<h2>Cono Sur riestling</h2>
 								</li>
