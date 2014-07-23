3f0075cee8622da1a247e5aca0020b976eb49205
invabot <invabot>
2014-07-22 22:36:41 -0400

    link

--- php-frontend/apps/frontend/modules/recipes/templates/indexSuccess.php
+++ php-frontend/apps/frontend/modules/recipes/templates/indexSuccess.php
@@ -39,6 +39,7 @@
 								<li class="recipe-name txt-center">
 									<h1>Paula Troncoso</h1> 
 									<p>CORDERO CON FRITAS EN SALSA FRAMBUESA</p>
+									<a href="">View Recipe</a> 
 								</li>
 								<li class="recipe-wine txt-center light">
 									<span class="icon-glass"></span>
@@ -59,6 +60,7 @@
 								<li class="recipe-name txt-center">
 									<h1>Paula Troncoso</h1> 
 									<p>CORDERO CON FRITAS EN SALSA FRAMBUESA</p>
+									<a href="">View Recipe</a> 
 								</li>
 								<li class="recipe-wine txt-center light">
 									<span class="icon-glass"></span>
@@ -75,6 +77,7 @@
 								<li class="recipe-name txt-center">
 									<h1>Paula Troncoso</h1> 
 									<p>CORDERO CON FRITAS EN SALSA FRAMBUESA</p>
+									<a href="">View Recipe</a> 
 								</li>
 								<li class="recipe-wine txt-center light">
 									<span class="icon-glass"></span>
@@ -90,19 +93,18 @@
 
 						<div class="col-md-6">
 
-							<a href="recipe_detail.html">
-								<ul class="recipe-item violet">
-									<li class="recipe-img" style="background-image: url(<?=public_path("img/recipe01.jpg");?>)"></li>
-									<li class="recipe-name txt-center">
-										<h1>Paula Troncoso</h1> 
-										<p>CORDERO CON FRITAS EN SALSA FRAMBUESA</p>
-									</li>
-									<li class="recipe-wine txt-center light">
-										<span class="icon-glass"></span>
-										<h2>Cono Sur riestling</h2>
-									</li>
-								</ul>
-							</a>
+							<ul class="recipe-item violet">
+								<li class="recipe-img" style="background-image: url(<?=public_path("img/recipe01.jpg");?>)"></li>
+								<li class="recipe-name txt-center">
+									<h1>Paula Troncoso</h1> 
+									<p>CORDERO CON FRITAS EN SALSA FRAMBUESA</p>
+									<a href="">View Recipe</a> 
+								</li>
+								<li class="recipe-wine txt-center light">
+									<span class="icon-glass"></span>
+									<h2>Cono Sur riestling</h2>
+								</li>
+							</ul>
 						</div>
 
 						<div class="col-md-6">
@@ -112,6 +114,7 @@
 								<li class="recipe-name txt-center">
 									<h1>Paula Troncoso</h1> 
 									<p>CORDERO CON FRITAS EN SALSA FRAMBUESA</p>
+									<a href="">View Recipe</a> 
 								</li>
 								<li class="recipe-wine txt-center light">
 									<span class="icon-glass"></span>
