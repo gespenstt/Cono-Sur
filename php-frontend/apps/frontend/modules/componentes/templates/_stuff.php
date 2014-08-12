a1f8ac5bb47e915c289e4b97b1681b30d240b3ef
invabot <invabot>
2014-08-11 22:32:57 -0400

    cambio titulo

--- php-frontend/apps/frontend/modules/componentes/templates/_stuff.php
+++ php-frontend/apps/frontend/modules/componentes/templates/_stuff.php
@@ -158,7 +158,7 @@ please write to <a href="mailto:webmanager@conosurwinery.cl">webmanager@conosurw
     <div class="modal-content">
       <div class="modal-header">
         <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
-        <h4 class="modal-title" id="myModalLabel">Vote <span id="titulo-receta"></span></h4>
+        <h2>YOU ARE VOTING FOR:</h2> <h4 class="modal-title" id="myModalLabel"><span id="titulo-receta"></span></h4>
       </div>
         
       <div class="modal-body">
