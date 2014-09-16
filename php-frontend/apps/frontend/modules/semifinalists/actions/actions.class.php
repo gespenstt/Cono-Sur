ff5f6d488b3374b3545c4be8b7691adbbeb41773
Jaime Vasquez <rodrigoxv@gmail.com>
2014-09-16 18:27:03 -0300

    Update semifinalistas

--- php-frontend/apps/frontend/modules/semifinalists/actions/actions.class.php
+++ php-frontend/apps/frontend/modules/semifinalists/actions/actions.class.php
@@ -17,6 +17,66 @@ class semifinalistsActions extends sfActions
   */
   public function executeIndex(sfWebRequest $request)
   {
-    $this->forward('default', 'module');
+      $cookie = unserialize($_COOKIE["conosur"]);
+      $funciones = new funciones();
+      $id_idioma = $funciones->mercheKeyIdioma($cookie["id"]);
+      if($id_idioma>1&&$id_idioma<5){
+          $id_idioma = $id_idioma;
+      }else{
+          $id_idioma = 5;        
+      }
+      $c = new Criteria();
+      $c->clearSelectColumns();
+      $c->addSelectColumn(PaginaPeer::PAG_IDENTIFICADOR);
+      $c->addSelectColumn(SeccionPeer::SEC_IDENTIFICADOR);
+      $c->addSelectColumn(DiccionarioPeer::DIC_TEXTO);
+      $c->addSelectColumn(ParametroPeer::PAR_IDENTIFICADOR);
+      $c->addJoin(PaginaPeer::PAG_ID, SeccionPeer::PAG_ID);
+      $c->addJoin(SeccionPeer::SEC_ID, ParametroPeer::SEC_ID);
+      $c->addJoin(ParametroPeer::PAR_ID, DiccionarioPeer::PAR_ID);
+      $c->add(DiccionarioPeer::IDI_ID,$id_idioma);
+      $c->add(PaginaPeer::PAG_ID,8);
+      
+      $resC = DiccionarioPeer::doSelectStmt($c);
+      $array_out = array();
+      if($resC){
+          while($row = $resC->fetch()){
+              $array_out[$row["SEC_IDENTIFICADOR"]][$row["PAR_IDENTIFICADOR"]]=  nl2br($row["DIC_TEXTO"]);
+          }
+      }else{
+          echo "Error :(";
+          exit;
+      }
+      
+      $this->diccionario = $array_out;  
+      
+      
+      //IRLANDA
+      $c = new Criteria();
+      $c->add(RecetaPeer::REC_ELIMINADO,0);
+      $c->addDescendingOrderByColumn(RecetaPeer::CREATED_AT);
+      $c->add(RecetaPeer::REC_PAIS,2);
+      $c->add(RecetaPeer::REC_ESTADO,1);
+      $c->add(RecetaPeer::REC_SEMI,1);
+      $resC = RecetaPeer::doSelect($c);  
+      $this->irlanda = $resC;
+      //SUECIA
+      $d = new Criteria();
+      $d->add(RecetaPeer::REC_ELIMINADO,0);
+      $d->addDescendingOrderByColumn(RecetaPeer::CREATED_AT);
+      $d->add(RecetaPeer::REC_PAIS,3);
+      $d->add(RecetaPeer::REC_ESTADO,1);
+      $d->add(RecetaPeer::REC_SEMI,1);
+      $resD = RecetaPeer::doSelect($d);      
+      $this->suecia = $resD;
+      //FINLANDIA
+      $e = new Criteria();
+      $e->add(RecetaPeer::REC_ELIMINADO,0);
+      $e->addDescendingOrderByColumn(RecetaPeer::CREATED_AT);
+      $e->add(RecetaPeer::REC_PAIS,4);
+      $e->add(RecetaPeer::REC_ESTADO,1);
+      $e->add(RecetaPeer::REC_SEMI,1);
+      $resE = RecetaPeer::doSelect($e);   
+      $this->finlandia = $resE;
   }
 }
---
+++ php-frontend/apps/frontend/modules/semifinalists/templates/indexSuccess.php
@@ -0,0 +1,201 @@
+<?php
+    $array_diccionario = $sf_data->getRaw("diccionario");
+?>
+				<div class="container-fluid">
+
+
+					<div class="row show-grid">
+
+						<div class="col-md-12">
+						
+							<div class="bubble">Finland</div>
+							
+						</div>
+							
+					</div>
+                                                    
+                                    <?php 
+                                    $count = 0;
+                                    $count_color = 0;
+                                    $array_color = array(
+                                      "green","dark-grey","yellow","red"
+                                    );
+                                    
+                                    foreach($finlandia as $re){ 
+                                        $resto = $count % 2;
+                                    ?>
+                                        <?php if($resto==0){ ?>
+					<div class="row show-grid">
+                                        <?php } ?>
+						<div class="col-md-6">
+
+							<ul class="recipe-item <?=$array_color[$count_color];?>">
+								<li class="recipe-img" style="background-image: url(<?=public_path("uploads/".$re->getRecImagen());?>)"></li>
+								<li class="recipe-name txt-center">
+									<h1><?=$re->getRecNombreBlogger();?></h1> 
+									<p><?php
+                                                                        $nreceta = $re->getRecNombreReceta();
+                                                                        if(strlen($nreceta) > 60){
+                                                                            $nreceta = substr($nreceta, 0, 60)."...";
+                                                                        }
+                                                                        echo $nreceta;
+                                                                        ?></p>
+									<a href="<?=url_for("recipes/detail/?id=".$re->getRecId());?>" class="link-view-recipe"><?=$array_diccionario["panel_b"]["view_recipe"];?></a> 
+								</li>
+								<li class="recipe-wine txt-center light">
+									<a data-receta="<?=$re->getRecNombreReceta();?>" data-id="<?=$re->getRecId();?>" class="link-vote modal-vote"><?=$array_diccionario["panel_b"]["vote_for"];?></a> 
+								</li>
+							</ul>
+							
+						</div>
+
+                                        <?php if($resto==1){ ?>
+                                        </div>
+                                        <?php 
+                                              $count_color++;
+                                              if($count_color>3){
+                                                  $count_color=0;
+                                              }
+                                        } 
+                                        ?>
+                                                    
+                                    <?php 
+                                    $count++;
+                                    } 
+                                    ?>
+
+				</div>
+
+
+
+				<div class="container-fluid">
+
+
+					<div class="row show-grid">
+
+						<div class="col-md-12">
+						
+							<div class="bubble">Ireland</div>
+							
+						</div>
+							
+					</div>
+                                                    
+                                    <?php 
+                                    $count = 0;
+                                    $count_color = 0;
+                                    $array_color = array(
+                                      "green","dark-grey","yellow","red"
+                                    );
+                                    
+                                    foreach($irlanda as $re){ 
+                                        $resto = $count % 2;
+                                    ?>
+                                        <?php if($resto==0){ ?>
+					<div class="row show-grid">
+                                        <?php } ?>
+						<div class="col-md-6">
+
+							<ul class="recipe-item <?=$array_color[$count_color];?>">
+								<li class="recipe-img" style="background-image: url(<?=public_path("uploads/".$re->getRecImagen());?>)"></li>
+								<li class="recipe-name txt-center">
+									<h1><?=$re->getRecNombreBlogger();?></h1> 
+									<p><?php
+                                                                        $nreceta = $re->getRecNombreReceta();
+                                                                        if(strlen($nreceta) > 60){
+                                                                            $nreceta = substr($nreceta, 0, 60)."...";
+                                                                        }
+                                                                        echo $nreceta;
+                                                                        ?></p>
+									<a href="<?=url_for("recipes/detail/?id=".$re->getRecId());?>" class="link-view-recipe"><?=$array_diccionario["panel_b"]["view_recipe"];?></a> 
+								</li>
+								<li class="recipe-wine txt-center light">
+									<a data-receta="<?=$re->getRecNombreReceta();?>" data-id="<?=$re->getRecId();?>" class="link-vote modal-vote"><?=$array_diccionario["panel_b"]["vote_for"];?></a> 
+								</li>
+							</ul>
+							
+						</div>
+
+                                        <?php if($resto==1){ ?>
+                                        </div>
+                                        <?php 
+                                              $count_color++;
+                                              if($count_color>3){
+                                                  $count_color=0;
+                                              }
+                                        } 
+                                        ?>
+                                                    
+                                    <?php 
+                                    $count++;
+                                    } 
+                                    ?>
+
+				</div>
+
+
+
+				<div class="container-fluid">
+
+
+					<div class="row show-grid">
+
+						<div class="col-md-12">
+						
+							<div class="bubble">Sweden</div>
+							
+						</div>
+							
+					</div>
+                                                    
+                                    <?php 
+                                    $count = 0;
+                                    $count_color = 0;
+                                    $array_color = array(
+                                      "green","dark-grey","yellow","red"
+                                    );
+                                    
+                                    foreach($suecia as $re){ 
+                                        $resto = $count % 2;
+                                    ?>
+                                        <?php if($resto==0){ ?>
+					<div class="row show-grid">
+                                        <?php } ?>
+						<div class="col-md-6">
+
+							<ul class="recipe-item <?=$array_color[$count_color];?>">
+								<li class="recipe-img" style="background-image: url(<?=public_path("uploads/".$re->getRecImagen());?>)"></li>
+								<li class="recipe-name txt-center">
+									<h1><?=$re->getRecNombreBlogger();?></h1> 
+									<p><?php
+                                                                        $nreceta = $re->getRecNombreReceta();
+                                                                        if(strlen($nreceta) > 60){
+                                                                            $nreceta = substr($nreceta, 0, 60)."...";
+                                                                        }
+                                                                        echo $nreceta;
+                                                                        ?></p>
+									<a href="<?=url_for("recipes/detail/?id=".$re->getRecId());?>" class="link-view-recipe"><?=$array_diccionario["panel_b"]["view_recipe"];?></a> 
+								</li>
+								<li class="recipe-wine txt-center light">
+									<a data-receta="<?=$re->getRecNombreReceta();?>" data-id="<?=$re->getRecId();?>" class="link-vote modal-vote"><?=$array_diccionario["panel_b"]["vote_for"];?></a> 
+								</li>
+							</ul>
+							
+						</div>
+
+                                        <?php if($resto==1){ ?>
+                                        </div>
+                                        <?php 
+                                              $count_color++;
+                                              if($count_color>3){
+                                                  $count_color=0;
+                                              }
+                                        } 
+                                        ?>
+                                                    
+                                    <?php 
+                                    $count++;
+                                    } 
+                                    ?>
+
+				</div>
