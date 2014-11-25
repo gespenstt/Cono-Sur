8a7855cb8e04ff81b1f6675da7d1ad5bfd6805b7
Jaime Vasquez <rodrigoxv@gmail.com>
2014-11-24 21:46:44 -0300

    Update winner

---
+++ php-frontend/apps/frontend/modules/winner/actions/actions.class.php
@@ -0,0 +1,84 @@
+<?php
+
+/**
+ * recipes actions.
+ *
+ * @package    Cono Sur
+ * @subpackage recipes
+ * @author     Your name here
+ * @version    SVN: $Id: actions.class.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
+ */
+class recipesActions extends sfActions
+{
+ /**
+  * Executes index action
+  *
+  * @param sfRequest $request A request object
+  */
+  public function executeIndex(sfWebRequest $request)
+  {
+      $cookie = unserialize($_COOKIE["conosur"]);
+      $funciones = new funciones();
+      $id_idioma = $funciones->mercheKeyIdioma($cookie["id"]);
+      if($id_idioma>1&&$id_idioma<5){
+          $id_idioma = $id_idioma;
+      }else{
+          $id_idioma = 5;          
+      }
+      $c = new Criteria();
+      $c->clearSelectColumns();
+      $c->addSelectColumn(PaginaPeer::PAG_IDENTIFICADOR);
+      $c->addSelectColumn(SeccionPeer::SEC_IDENTIFICADOR);
+      $c->addSelectColumn(DiccionarioPeer::DIC_TEXTO);
+      $c->addSelectColumn(ParametroPeer::PAR_IDENTIFICADOR);
+      $c->addJoin(PaginaPeer::PAG_ID, SeccionPeer::PAG_ID);
+      $c->addJoin(SeccionPeer::SEC_ID, ParametroPeer::SEC_ID);
+      $c->addJoin(ParametroPeer::PAR_ID, DiccionarioPeer::PAR_ID);
+      $c->add(DiccionarioPeer::IDI_ID,$id_idioma);
+      $c->add(PaginaPeer::PAG_ID,9);
+      
+      $resC = DiccionarioPeer::doSelectStmt($c);
+      $array_out = array();
+      if($resC){
+          while($row = $resC->fetch()){
+              $array_out[$row["SEC_IDENTIFICADOR"]][$row["PAR_IDENTIFICADOR"]]=  nl2br($row["DIC_TEXTO"]);
+          }
+      }else{
+          echo "Error :(";
+          exit;
+      }
+      
+      $this->diccionario = $array_out; 
+      
+      $id_receta = $request->getParameter("id");
+      
+      $cre = new Criteria();
+      $cre->add(RecetaPeer::REC_GANADOR,1);
+      $cre->add(RecetaPeer::REC_ELIMINADO,0);
+      $cre->add(RecetaPeer::REC_ESTADO,1);
+      $resC = RecetaPeer::doSelectOne($cre);
+      if(!$resC){
+          $this->redirect("home/index");
+      }
+      
+      $this->receta = $resC;
+      
+      $this->modal = false;
+      $modal = $request->getParameter("voto");
+      if(!empty($modal)){
+          switch(strtolower($modal)){
+              case "ok":
+                  $this->modal = true;
+                  $this->msg = "Congratulations! You have successfully voted!";
+                  break;
+              case "nok":
+                  $this->modal = true;
+                  $this->msg = "Oops, something did not work, please try to vote again. If you continue to have problems, please write to webmanager@conosurwinery.cl.";
+                  break;
+          }
+      }
+      $this->ref = $_SERVER["HTTP_REFERER"] ? $_SERVER["HTTP_REFERER"] : $_SERVER["SCRIPT_NAME"]."/recipes/index";
+      
+  
+  }
+}
---
+++ php-frontend/apps/frontend/modules/winner/templates/indexSuccess.php
@@ -0,0 +1,72 @@
+<?php
+    $array_diccionario = $sf_data->getRaw("diccionario");
+    //slot('social_title', json_encode($receta->getRecNombreReceta()));
+    //slot('social_desc', json_encode($receta->getRecInstrucciones()));
+    slot('social_title','"'.$receta->getRecNombreReceta().'"');
+    slot('social_desc','"'.$receta->getRecInstrucciones().'"');
+    slot('social_img', "http://bloggercompetition.conosur.com/uploads/".$receta->getRecImagen());
+?>
+				<div class="container-fluid">
+
+					<div class="row show-grid">
+
+						<div class="col-md-4">
+
+									<span class="blogger-name green txt-center">
+								
+                                                                            <h1><?=$receta->getRecNombreBlogger();?></h1>
+                                                                            <?php
+                                                                                $url_bloger = $receta->getRecUrlBlogger();
+                                                                                if(strpos(strtolower($url_bloger), "http://")===FALSE){
+                                                                                    $url_bloger = "http://".$url_bloger;
+                                                                                }
+                                                                            ?>
+                                                                            <a href="<?=$url_bloger;?>" target="_blank"><?=$array_diccionario["panel_a"]["view_blog"];?></a>
+
+									</span>
+									
+
+                                                    <span style="background-image: url(<?=public_path("uploads/".$receta->getRecImagen());?>)" class="recipe-picture"></span>
+
+                                                        <span class="picture-info"><?=$receta->getRecVino();?></span>
+                                                        
+                                                        <!-- share -->
+                                                        <?php
+                                                            $url_share = "http://bloggercompetition.conosur.com/index.php/recipes/detail/id/".$receta->getRecId();
+                                                        ?>
+                                                        <a href="https://www.facebook.com/sharer/sharer.php?u=<?=urlencode($url_share);?>" target="_blank" class="share-facebook">FACEBOOK</a> 
+                                                        <a href="https://twitter.com/home?status=<?=urlencode($url_share);?>" target="_blank" class="share-twitter">TWITTER</a>
+                                                        
+                                                        <!-- /share -->
+
+							<a href="<?=$ref;?>"><p><span class="btn btn-back pull-right"><?=$array_diccionario["panel_a"]["back_to"];?></span></p></a>
+
+						</div>
+
+					  	<div class="col-md-8">
+
+                                                    <h4><?=$receta->getRecNombreReceta();?></h4>
+
+					  		<h3><?=$array_diccionario["panel_b"]["ingredients"];?></h3>
+
+					  		<ul class="list-unstyled">
+
+							 	<?=nl2br(html_entity_decode($receta->getRecIngredientes()));?>
+
+							</ul>
+
+							<h3><?=$array_diccionario["panel_b"]["instructions"];?></h3>
+
+                                                        <p class="text-left">
+                                                            <?=nl2br(html_entity_decode($receta->getRecInstrucciones()));?>
+                                                        </p>
+
+					  		
+
+					  	</div>
+
+					</div>
+
+				</div>
+				
+
