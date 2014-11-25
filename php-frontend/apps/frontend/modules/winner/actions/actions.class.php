1b78e6e2be4590da9fcc0baed0e99b84bb5695e5
Jaime Vasquez <rodrigoxv@gmail.com>
2014-11-24 21:53:11 -0300

    fix class

--- php-frontend/apps/frontend/modules/winner/actions/actions.class.php
+++ php-frontend/apps/frontend/modules/winner/actions/actions.class.php
@@ -8,7 +8,7 @@
  * @author     Your name here
  * @version    SVN: $Id: actions.class.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
  */
-class recipesActions extends sfActions
+class winnerActions extends sfActions
 {
  /**
   * Executes index action
