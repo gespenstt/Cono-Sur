13a7e8ae2531b54eb3617773e099d718a337678d
invabot <invabot>
2015-04-27 14:55:40 -0300

    single post

--- theme/blogger/single.php
+++ theme/blogger/single.php
@@ -1,7 +1,42 @@
 <?php
     get_header(); 
 ?>
-CONTENIDO SINGLE
+<div class="container-fluid">
+
+					<div class="row show-grid">
+
+						<div class="col-md-8">		
+
+							<span style="background-image: url(../img/img-04.jpg);" class="recipe-picture"></span>
+
+						</div>
+
+					  	<div class="col-md-4">
+
+					  		<h4>Find Inspiration: Interactive Storytelling</h4>
+
+					  		<h3>by elainemcmillion March 10, 2015  Leave a comment</h3>
+
+					  		<p>Education & Training, Hollow, News and Links, Uncategorized
+arte, awards, best interactive websites, funding, interactive documentary, interactive media, list of resources, national film board of canada, nfb, storytelling, submarine channel, sxsw, tribeca, webbys
+</p>
+
+						</div>
+
+						<div class="col-md-12">
+
+					  		<p>A friend wrote me asking for resources to find/discover/learn about interactive storytelling. I compiled my list of go-to links and sent her an email, but decided that it may be helpful for all of you too!</p>
+
+					  		<p>So below, I have listed out the sites that I like to go to for updates in interactive storytelling. There are certainly many more (add the ones you love in the comments section) but these are my favorites, and have a bit of a non-fiction slant to them.</p>
+
+					  		<p>Kuumenna mieluiten valurautaisella paistinpannulla n. puolen senttimetrin kerros rypsiöljyä. Pannu saa olla melko kuuma. Nostele ruokalusikalla raastesekoitusta pannulle, paina litteäksi ja muotoile röstit lusikalla halu- amasi mallisiksi. Paista kullanruskeiksi ja rapeiksi molemmilta puolilta, n. 2-3 minuuttia per puoli.</p>
+					  		
+
+					  	</div>
+
+					</div>
+
+				</div>
 <?php 
     get_footer();
 ?>
