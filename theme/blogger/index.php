2ff78610e3121430053bf309c8b592208d6cd268
invabot <invabot>
2015-04-27 14:41:10 -0300

    home blog

--- theme/blogger/index.php
+++ theme/blogger/index.php
@@ -1,7 +1,125 @@
 <?php
     get_header(); 
 ?>
-CONTENIDO 
+				<div class="container-fluid">
+
+					<div class="row show-grid">
+
+						<div class="col-md-4">
+							<figure class="effect-lily">
+								<img src="img/img-04.jpg" alt="img12"/>
+								<figcaption>
+									<div>
+										<h2>Nice <span>Lily</span></h2>
+										<p>Lily likes to play with crayons and pencils</p>
+									</div>
+									<a href="#">View more</a>
+								</figcaption>			
+							</figure>
+						</div>
+
+					  	<div class="col-md-4 no-padding">
+					  		<figure class="effect-lily especial">
+								<img src="img/img-05.jpg" alt="img12"/>
+								<figcaption>
+									<div>
+										<h2>Nice <span>Lily</span></h2>
+										<p>Lily likes to play with crayons and pencils</p>
+									</div>
+									<a href="#">View more</a>
+								</figcaption>			
+							</figure>
+					  	</div>
+
+					  	<div class="col-md-4">
+					  		<figure class="effect-lily">
+								<img src="img/img-06.jpg" alt="img12"/>
+								<figcaption>
+									<div>
+										<h2>Nice <span>Lily</span></h2>
+										<p>Lily likes to play with crayons and pencils</p>
+									</div>
+									<a href="#">View more</a>
+								</figcaption>			
+							</figure>
+					  	</div>
+
+					  	<div class="col-md-4">
+							<figure class="effect-lily">
+								<img src="img/img-04.jpg" alt="img12"/>
+								<figcaption>
+									<div>
+										<h2>Nice <span>Lily</span></h2>
+										<p>Lily likes to play with crayons and pencils</p>
+									</div>
+									<a href="#">View more</a>
+								</figcaption>			
+							</figure>
+						</div>
+
+					  	<div class="col-md-4 no-padding">
+					  		<figure class="effect-lily especial">
+								<img src="img/img-05.jpg" alt="img12"/>
+								<figcaption>
+									<div>
+										<h2>Nice <span>Lily</span></h2>
+										<p>Lily likes to play with crayons and pencils</p>
+									</div>
+									<a href="#">View more</a>
+								</figcaption>			
+							</figure>
+					  	</div>
+
+					  	<div class="col-md-4">
+					  		<figure class="effect-lily">
+								<img src="img/img-06.jpg" alt="img12"/>
+								<figcaption>
+									<div>
+										<h2>Nice <span>Lily</span></h2>
+										<p>Lily likes to play with crayons and pencils</p>
+									</div>
+									<a href="#">View more</a>
+								</figcaption>			
+							</figure>
+					  	</div>
+
+					</div>					
+					
+				</div>
+
+				<div class="footer container-fluid">
+
+					<div class="row show-grid">
+
+						<div class="col-md-2">
+							© 2014 Cono Sur
+						</div>
+
+					  	<div class="col-md-3">
+					  		participating countries: <img src="img/country.png" alt="">
+					  	</div>
+
+					  	<div class="col-md-2">
+					  		<a href="http://www.conosur.com" target="_blank">www.conosur.com</a>
+					  	</div>
+
+					  	<div class="col-md-3">
+					  		<a href="mailto:webmanager@conosurwinery.cl">contact: webmanager@conosurwinery.cl</a>
+					  	</div>
+
+					  	<div class="col-md-2">
+
+					  		<a href="https://twitter.com/ConoSurWines" class="twitter"></a> <a href="https://www.facebook.com/conosurwinery?ref=hl" class="facebook"></a> <a href="http://instagram.com/vinaconosur" class="instagram"></a> <a href="http://www.pinterest.com/ConoSurWines/" class="pinterest"></a>
+					  		
+					  	</div>
+
+
+					</div>
+
+				</div>
+
+
+			</div><!-- /End Container -->
 <?php 
     get_footer();
 ?>
