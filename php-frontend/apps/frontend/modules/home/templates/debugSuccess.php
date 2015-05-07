0f470ed9eacfb12533a190084e97302f74b9c7c9
Jaime Vasquez <rodrigoxv@gmail.com>
2015-05-07 16:45:14 -0300

    debug idioma

--- php-frontend/apps/frontend/modules/home/templates/debugSuccess.php
+++ php-frontend/apps/frontend/modules/home/templates/debugSuccess.php
@@ -1,9 +1,11 @@
 <div style="padding-top: 70px;">
     Seleccione el idioma: <br><br>
-    <a href="<?=url_for("home/debug/?lang=1");?>" class="btn btn-default">Ingles UK</a> 
     <a href="<?=url_for("home/debug/?lang=2");?>" class="btn btn-default">Ingles Irlanda</a> 
     <a href="<?=url_for("home/debug/?lang=3");?>" class="btn btn-default">Sueco</a> 
-    <a href="<?=url_for("home/debug/?lang=4");?>" class="btn btn-default">Finlandes</a> 
+    <a href="<?=url_for("home/debug/?lang=9");?>" class="btn btn-default">Canada</a> 
+    <a href="<?=url_for("home/debug/?lang=6");?>" class="btn btn-default">Japon</a> 
+    <a href="<?=url_for("home/debug/?lang=7");?>" class="btn btn-default">Chile</a> 
+    <a href="<?=url_for("home/debug/?lang=8");?>" class="btn btn-default">USA</a> 
     <a href="<?=url_for("home/debug/?reset=ok");?>" class="btn btn-warning">RESET</a>
     <a href="<?=url_for("home/debug/?resetlegal=ok");?>" class="btn btn-warning">RESET Legal</a> 
     <?php if(!empty($msg)): ?>
