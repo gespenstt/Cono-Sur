4d063aa559bb8f9e50a5ac4780e23d4110dff37d
Jaime Vasquez <rodrigoxv@gmail.com>
2015-05-11 16:11:27 -0300

    header footer

--- theme/blogger/header.php
+++ theme/blogger/header.php
@@ -1,4 +1,14 @@
 <?php
-    $contHeader = file_get_contents("http://conosur.ratamonkey.com/web/index.php/componentes/header");
-    echo $contHeader;
+    //$contHeader = file_get_contents("http://conosur.ratamonkey.com/web/index.php/componentes/header");
+    //echo $contHeader;
+    $url = 'http://conosur.ratamonkey.com/web/index.php/componentes/header';
+    $curl = curl_init();
+    curl_setopt($curl, CURLOPT_URL, $url);
+    curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+    // you may set this options if you need to follow redirects. Though I didn't get any in your case
+    curl_setopt($curl, CURLOPT_FOLLOWLOCATION, true);
+    $content = curl_exec($curl);
+    curl_close($curl);
+
+    $html = str_get_html($content);
 ?>
