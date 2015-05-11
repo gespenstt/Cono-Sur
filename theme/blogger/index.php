75b90d92b5ce9939df1ae16e836678beff3e83d2
Jaime Vasquez <rodrigoxv@gmail.com>
2015-05-11 17:57:09 -0300

    funco

--- theme/blogger/index.php
+++ theme/blogger/index.php
@@ -25,10 +25,12 @@
                 <?php
                     endwhile;
                 ?>
-                <div class="nav-previous alignleft"><?php next_posts_link( 'Older posts' ); ?></div>
-<div class="nav-next alignright"><?php previous_posts_link( 'Newer posts' ); ?></div>
                 
-            </div>					
+            </div>
+            <div class="row show-grid">
+                <div class="nav-previous alignleft"><?php next_posts_link( 'Older posts' ); ?></div>
+                <div class="nav-next alignright"><?php previous_posts_link( 'Newer posts' ); ?></div>                
+            </div>
 
     </div>
 
