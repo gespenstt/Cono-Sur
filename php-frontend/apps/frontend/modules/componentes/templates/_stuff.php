491c1f972ecc659c68a4d40a98c39631059fef22
Pablo Gonzalez Dabed <invasor@gmail.com>
2015-05-14 11:27:33 -0300

    fix

--- php-frontend/apps/frontend/modules/componentes/templates/_stuff.php
+++ php-frontend/apps/frontend/modules/componentes/templates/_stuff.php
@@ -277,7 +277,7 @@ please write to <a href="mailto:webmanager@conosurwinery.cl">webmanager@conosurw
 							
 			</div>
 
-			<div class="col-md-7 contest-details">
+			<div class="col-md-7">
 
 				<p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no seaest Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua, ugo consetetur Lorem Ipsum.</p>
 					  		
